9fd08d7c7c3440cecac18e4a1cb08845108f1673
Leslie Brown <lkb@alum.mit.edu>
2015-07-22 11:18:27 -0400

    Adding login / logout functionality.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    if session[:user_id].nil?
+      redirect_to login_path
+    else
+      @user = User.find(session[:user_id])
+    end
+  end
+
   def new
     @user = User.new
   end
