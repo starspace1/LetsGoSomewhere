de1bb1bf283dae33b3047a6b524872023dfc5de3
Leslie Brown <lkb@alum.mit.edu>
2015-07-22 15:21:58 -0400

    Users can view their trips and all users in a trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,7 @@
 class TripsController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
+    @trips = @user.trips
   end
 
   def new
@@ -11,6 +13,10 @@ class TripsController < ApplicationController
     @user = User.find(params[:user_id])
     @trip = @user.trips.create(trip_params)
     redirect_to root_path, notice: "Success! Created new trip #{@trip.name}."
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
   end
 
   def trip_params
