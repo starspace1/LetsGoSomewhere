b6f534e2e9b025efda446855512fb26c5b006bf2
Leslie Brown <lkb@alum.mit.edu>
2015-07-26 12:21:59 -0400

    adding list of potential destination to trip page

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,14 @@ class Trip < ActiveRecord::Base
   validates :name, presence: true
   has_many :groups
   has_many :users, through: :groups
+
+  def destination_ids
+    # TODO make less C++ish
+    user_dests = []
+    users.each do |user|
+      user_dests << user.destination_ids
+    end
+    user_dests.inject { |result, element| result & element }
+  end
+
 end
