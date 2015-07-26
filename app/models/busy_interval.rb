d0f44b1dc3f51a41e15fe01d8b89c04e90ddecd5
Leslie Brown <lkb@alum.mit.edu>
2015-07-26 14:31:04 -0400

    adding basic date/time-interval merge logic

--- app/models/busy_interval.rb
+++ app/models/busy_interval.rb
@@ -1,4 +1,16 @@
 class BusyInterval < ActiveRecord::Base
   validates_presence_of :start_time, :end_time
   belongs_to :user
+
+  def adjacent_to? date
+    start_time == date + 1 || end_time == date - 1
+  end
+
+  def extend_interval date
+    if start_time == date + 1
+      update_attribute(:start_time, date)
+    elsif end_time == date - 1
+     update_attribute(:end_time, date)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,16 @@ class User < ActiveRecord::Base
   has_many :busy_intervals
 
   def mark_as_busy date
+    # If any contain this date, do nothing
+    return if busy_on? date
+    # Look through existing busy intervals for this user
+    busy_intervals.each do |interval|
+      # If any are adjacent to this date, 
+      if interval.adjacent_to? DateTime.iso8601(date)
+        # Edit that interval, make this date the new start or end date for the interval
+        interval.extend_interval DateTime.iso8601(date)
+      end
+    end
     busy_intervals.create(start_time: DateTime.iso8601(date), end_time: DateTime.iso8601(date)) unless busy_on? date
   end
 
