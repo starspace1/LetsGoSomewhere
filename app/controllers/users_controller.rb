3a3b65cef319e06c022996fbec7b60cdd2815314
Leslie Brown <lkb@alum.mit.edu>
2015-07-28 11:40:44 -0400

    adding interests controller, refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,4 @@
 class UsersController < ApplicationController
-  def edit_destinations
-    @user = current_user
-  end
-
-  def update_destinations
-    @user = current_user
-    @user.update(params.require(:user).permit(:destination_ids => []))
-    redirect_to root_path
-  end
 
   def edit_dates
     @user = current_user
