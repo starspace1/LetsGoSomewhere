67ee1b7f8573bb6002789af1dd4bb706dd78e5ff
Leslie Brown <lkb@alum.mit.edu>
2015-08-03 19:07:54 -0400

    moving redirect uri to env variable
     need to learn best practice for this

--- config/initializers/google.rb
+++ config/initializers/google.rb
@@ -1,9 +1,8 @@
-# TODO account for redirect uri in production
 GOOGLE = { 
   client_id: ENV["GOOGLE_CLIENT_ID"],
   client_secret: ENV["GOOGLE_CLIENT_SECRET"],
   authorize_uri: "https://accounts.google.com/o/oauth2/auth",
-  redirect_uri: "http://localhost:3000/oauth/request_access_token",
+  redirect_uri: ENV["GOOGLE_REDIRECT_URI"],
   token_uri: "https://accounts.google.com/o/oauth2/token",
   application_name: "letsgosomewhere"
 }
