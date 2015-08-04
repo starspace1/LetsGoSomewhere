226433cc7f10768d624252bb8211747d4828419a
Leslie Brown <lkb@alum.mit.edu>
2015-08-03 21:42:00 -0400

    restricting who can view user information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    # Don't show information about a user that shares no trips with current user
+    if current_user.shares_trip_with?(@user) == false
+      redirect_to root_path, alert: "You are not authorized to view this user's information."
+    end
   end
 end
